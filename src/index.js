2a90f3e5894e7ff1601115c0fcd1e12b0b2fbb13
Jatematik <freerun_dimon@mail.ru>
2020-11-25 18:13:54 +0600

    feat: added popupDiscount

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@
 import test from './modules/test';
 import popupCall from './modules/popupCall';
 import sendForm from './modules/sendForm';
+import popupDiscount from './modules/popupDiscount';
 
 test();
 
 popupCall();
 
 sendForm();
+
+popupDiscount();
---
+++ src/modules/popupDiscount.js
@@ -0,0 +1,31 @@
+const popupDiscount = () => {
+    const discountBtn = document.querySelectorAll('.discount-btn'),
+        discountModal = document.querySelector('.popup-discount'),
+        inputs = discountModal.querySelectorAll('input');
+
+    discountBtn.forEach((item) => {
+        item.addEventListener('click', () => {
+            discountModal.style.display = 'block';
+        });
+    });
+
+    discountModal.addEventListener('click', (event) => {
+        let target = event.target;
+        if (target.matches('.popup-close')) {
+            inputs.forEach((item) => {
+                item.value = '';
+            });
+            discountModal.style.display = 'none';
+        } else {
+            target = target.closest('.popup-dialog');
+            if (!target) {
+                inputs.forEach((item) => {
+                    item.value = '';
+                });
+                discountModal.style.display = 'none';
+            }
+        }
+    });
+};
+
+export default popupDiscount;
