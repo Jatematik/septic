643fc486d4a4c120d3d728eda6ec07782a1864e1
Jatematik <freerun_dimon@mail.ru>
2020-11-25 18:50:53 +0600

    feat: added popupConsultation

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import popupCall from './modules/popupCall';
 import sendForm from './modules/sendForm';
 import popupDiscount from './modules/popupDiscount';
 import popupCheck from './modules/popupCheck';
+import popupConsultation from './modules/popupConsultation';
 
 test();
 
@@ -15,3 +16,5 @@ sendForm();
 popupDiscount();
 
 popupCheck();
+
+popupConsultation();
---
+++ src/modules/popupConsultation.js
@@ -0,0 +1,77 @@
+const popupConsultation = () => {
+    const popupConsultation = document.querySelector('.popup-consultation'),
+        consultationBtn = document.querySelector('.consultation-btn'),
+        formDirector = document.querySelector('.director-form'),
+        input = formDirector.querySelector('input');
+
+        const errorMessage = 'Ошибка, что-то пошло не так...',
+            loadMessage = 'Идет отправка сообщения...',
+            successMessage = 'Сообщение отправлено! Мы скоро с вами свяжемся!';
+
+        const statusMessage = document.createElement('div');
+        statusMessage.style.cssText = `font-size: 2rem;
+        color: white;
+        text-align: center;`;
+
+    formDirector.addEventListener('submit', (event) => {
+        event.preventDefault();
+        formDirector.appendChild(statusMessage);
+        statusMessage.textContent = loadMessage;
+
+        popupConsultation.style.display = 'block';
+
+        const formData = new FormData(formDirector);
+        let body = {};
+        formData.forEach((val, key) => {
+            body[key] = val;
+        });
+        postData(body, () => {
+            statusMessage.textContent = successMessage;
+            function deleteMessageTime(){
+                statusMessage.remove();
+                clearInterval(deleteMessage);
+            }
+            let deleteMessage = setInterval(deleteMessageTime, 5000);
+        }, (error) => {
+            statusMessage.textContent = errorMessage;
+            console.error(error);
+            function deleteMessageTime(){
+                statusMessage.remove();
+                clearInterval(deleteMessage);
+            }
+            let deleteMessage = setInterval(deleteMessageTime, 5000);
+        });
+        input.value = '';
+    });
+
+    const postData = (body, outputData, errorData) => {
+        const request = new XMLHttpRequest();
+        request.addEventListener('readystatechange', () => {
+            if (request.readyState !== 4) {
+                return;
+            }
+            if (request.status === 200) {
+                outputData();
+            } else {
+                errorData(request.status);
+            }
+        });
+        request.open('POST', './server.php');
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify(body));
+    };
+
+    popupConsultation.addEventListener('click', (event) => {
+        let target = event.target;
+        if (target.matches('.popup-close')) {
+            popupConsultation.style.display = 'none';
+        } else {
+            target = target.closest('.popup-dialog');
+            if (!target) {
+                popupConsultation.style.display = 'none';
+            }
+        }
+    });
+};
+
+export default popupConsultation;
